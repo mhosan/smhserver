0fe7eed47a46ecb11d88b3a043b76b34a162b581
mhosan <mhosan@gmail.com>
2022-06-10 00:20:50 -0300

    nada

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = app.listen(port, () => {
 );
 server.on("error", error => { console.log(error) });
 
-app.get('/api/users', (req, res) => {
+app.get('/users', (req, res) => {
     console.log('GET /api/users respondiendo');
     res.json({msg : "Hello fucking World de porqueria y sigo escribiendo!"});
 }
