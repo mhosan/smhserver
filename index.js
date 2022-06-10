3c93c8bf1e20fe7f6a78e5aa2c30988ff6147c83
mhosan <mhosan@gmail.com>
2022-06-09 23:31:31 -0300

    primero

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8090;
+const server = app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+}
+);
+server.on("error", error => { console.log(error) });
+
+app.get('/api/users', (req, res) => {
+    console.log('GET /api/users respondiendo');
+    res.json({msg : "Hello World"});
+}
+);
+app.post('/users', (req, res) => {
+    console.log('POST /api/users respondiendo');
+    const usuario = {
+        id: 1,
+        firstName: 'Juan',
+        lastName: 'Hardcodeado',
+        email: 'jhardcodeado@email.com',
+      };
+    res.json({usuario : usuario});
+})
