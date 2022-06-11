c18ac0566ca2a0296b8cb6ea6254d861a8247b31
mhosan <mhosan@gmail.com>
2022-06-11 19:16:21 -0300

    nada

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8091;
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
 
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 }
 );
-server.on("error", error => { console.log(error) });
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+//server.on("error", error => { console.log(error) });
 
 app.get('/users', (req, res) => {
     console.log('GET /api/users respondiendo');
-    res.json({msg : "versión 10"});
+    res.json({msg : "versión 11"});
 }
 );
 app.post('/users', (req, res) => {
     console.log('POST /api/users respondiendo');
     console.log(req.body)
    
-    setTimeout(() => {
+   /*  setTimeout(() => {
         const email = req.body.mail
         const password = req.body.password
         const tc = req.body.tc
         res.json({'email' : email, 'password': password, 'tc': tc});
     }
-    , 2000);
+    , 2000); */
 
     /* res.json({
         "email" : req.body.mail,
@@ -39,6 +39,8 @@ app.post('/users', (req, res) => {
         "password": req.body.password,
         "tc": req.body.tc
     }); */
-
+    const email = "";
+    email = req.body.mail;
+    res.json({'email' : email});
     
 });
