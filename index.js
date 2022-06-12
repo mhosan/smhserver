7cf742bcfe0cc2e99c7a9a7c9ee6183a5ab59c0d
mhosan <mhosan@gmail.com>
2022-06-12 09:44:26 -0300

    Heroku is wrong?

--- index.js
+++ index.js
@@ -1,44 +1,28 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8091;
-
 const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 }
 );
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-//server.on("error", error => { console.log(error) });
+server.on("error", error => { console.log(error) });
+
+const version = "15";
 
 app.get('/users', (req, res) => {
     console.log('GET /api/users respondiendo');
-    res.json({msg : "versión 15"});
-}
-);
+    res.json({ msg: `Respuesta al get desde la versión: ${version}` });
+});
+
 app.post('/users', (req, res) => {
     console.log('POST /api/users respondiendo');
     console.log(req.body)
-   
-   /*  setTimeout(() => {
-        const email = req.body.mail
-        const password = req.body.password
-        const tc = req.body.tc
-        res.json({'email' : email, 'password': password, 'tc': tc});
-    }
-    , 2000); */
-
-    /* res.json({
-        "email" : req.body.mail,
+    return res.json({
+        "versión": version,
+        "email": req.body.mail,
         "password": req.body.password,
         "tc": req.body.tc
-      }); */
-
-    //res.send(`mail: ${email}, password: ${password}, tc: ${tc}`);
-    /* return res.json({
-        "email" : req.body.mail,
-        "password": req.body.password,
-        "tc": req.body.tc
-    }); */
-    res.json({msg : "versión 15"});
-    
+    });
 });
